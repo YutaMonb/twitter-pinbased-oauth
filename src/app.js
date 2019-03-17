0b37773e9039ac9df9ff457bffbd2b81c5537579
yutamonb <yutamonb@gmail.com>
2019-03-17 11:43:57 +0900

    Refactoring

--- src/app.js
+++ src/app.js
@@ -3,44 +3,50 @@
 
 const OAuth = require('oauth').OAuth;
 
+const TwitterOauth = (consumerKey, consumerSecret) => {
+  const auth = new OAuth(
+    'https://twitter.com/oauth/request_token',
+    'https://twitter.com/oauth/access_token',
+    consumerKey,
+    consumerSecret,
+    '1.0a',
+    null,
+    'HMAC-SHA1'
+  );
 
-let TwitterOauth = (consumerKey, consumerSecret) => {
+  auth.getOAuthRequestToken((err, token, secret) => {
+    if (err) {
+      console.log(err.data);
+      process.exit(1);
+    }
+    this.token = token;
+    this.secret = secret;
+    console.log(`https://twitter.com/oauth/authorize?oauth_token=${token}`);
+    console.log('Please type PIN.');
+  });
 
-    let auth = new OAuth(
-        'https://twitter.com/oauth/request_token',
-        'https://twitter.com/oauth/access_token',
-        consumerKey, consumerSecret, '1.0a', null, 'HMAC-SHA1'
-    );
-
-    auth.getOAuthRequestToken((err, token, secret) => {
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', pin => {
+    auth.getOAuthAccessToken(
+      this.token,
+      this.secret,
+      pin,
+      (err, token, secret) => {
         if (err) {
-            console.log(err.data);
-            process.exit(1);
+          console.log(err.data);
+          process.exit(1);
         }
-        this.token = token;
-        this.secret = secret;
-        console.log(`https://twitter.com/oauth/authorize?oauth_token=${token}`);
-        console.log('Please type PIN.')
-    });
-
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('data', (pin) => {
-        auth.getOAuthAccessToken(this.token, this.secret, pin, (err, token, secret) => {
-            if (err) {
-                console.log(err.data);
-                process.exit(1);
-            }
-            console.log(`Token: ${token}`);
-            console.log(`Secret: ${secret}`);
-            process.exit(0);
-        });
-    });
-
-}
+        console.log(`Token: ${token}`);
+        console.log(`Secret: ${secret}`);
+        process.exit(0);
+      }
+    );
+  });
+};
 
 if (process.argv.length < 4) {
-    console.log('twitter-pinbased-oauth {consumerKey} {consumerSecret}');
-    process.exit(1);
+  console.log('twitter-pinbased-oauth {consumerKey} {consumerSecret}');
+  process.exit(1);
 }
 
 TwitterOauth(process.argv[2], process.argv[3]);
